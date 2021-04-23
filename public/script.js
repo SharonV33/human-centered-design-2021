9421ba50929fe81bdd308ec1bbdcf50d55d47b79
sharonV33 <sharonveldman33@gmail.com>
2021-04-23 11:10:14 +0200

    create buttons to give a chat message an emotion instead of using the camera. Still need to figure out how to stop these functions once a different option (either buttons or camera) has been selected

--- public/script.js
+++ public/script.js
@@ -3,26 +3,43 @@ const messages = document.querySelector('section ul')
 const input = document.querySelector('input')
 const video = document.querySelector('video')
 const canvas = document.querySelector('canvas')
+const useCamera = document.querySelector('#useCamera')
+const useButtons = document.querySelector('#useButtons')
 let emotion = 'neutral'
 
+
+//ask user for their name when the page loads
 window.addEventListener('load', () => {
-    // loadFacialRecognition()
     storeUsername()
+    useCamera.addEventListener('change', function() {
+        if (this.checked) {
+            loadFacialRecognition()
+        }
+    })
+    useButtons.addEventListener('change', function() {
+        if (this.checked) {
+            buttonEmotions()
+        }
+    })
 })
 
 document.querySelector('form').addEventListener('submit', (event) => {
+    //submit message and emit to all users using sockets
     event.preventDefault()
     let user = localStorage.getItem('currentUser')
     if (input.value && user) {
         socket.emit('message', { user: user , content : input.value, emotion,  })
         input.value = ''
     }
+    //make sure user gives their username
     else if (input.value && !user) {
         storeUsername()
     }
 })
 
+
 document.querySelector(".collapse").addEventListener("click", function () {
+    //collapse learning section
         this.classList.toggle("active")
         const content = document
             .querySelector('.learningMessages')
@@ -34,6 +51,9 @@ document.querySelector(".collapse").addEventListener("click", function () {
     })
 
 socket.on('message', function(message) {
+    //on receiving a message, create a new li with the user and message in it
+    //then colour the message and give it the correct font, these colours and fonts are
+    //declared in custom properties in CSS
     const element = document.createElement('li')
     const user = localStorage.getItem('currentUser')
     element.textContent = `${message.user}: ${message.content}`
@@ -51,13 +71,59 @@ function storeUsername () {
     if (!localStorage.getItem('currentUser')) {
         //give the user a prompt to enter it and store it into locatsorage
         const username = prompt("Voer uw naam in:", "")
-        if (username == null || username == "") {
+        if (username == null || username === "") {
 
         } else {
             localStorage.setItem('currentUser', username)
         }
     }
-        console.log('current user', localStorage.getItem('currentUser'))
+}
+
+function buttonEmotions () {
+    console.log('check buttons')
+    const happy = document.querySelector('#happyMessage')
+    const sad = document.querySelector('#sadMessage')
+    const angry = document.querySelector('#angryMessage')
+    const surprised = document.querySelector('#surprisedMessage')
+    const disgusted = document.querySelector('#disgustedMessage')
+    const scared = document.querySelector('#fearfulMessage')
+    const neutral = document.querySelector('#neutralMessage')
+
+    happy.addEventListener('change', function() {
+        if (this.checked) {
+            emotion = 'happy'
+        }
+    })
+    sad.addEventListener('change', function() {
+        if (this.checked) {
+            emotion = 'sad'
+        }
+    })
+    angry.addEventListener('change', function() {
+        if (this.checked) {
+            emotion = 'angry'
+        }
+    })
+    surprised.addEventListener('change', function() {
+        if (this.checked) {
+            emotion = 'surprised'
+        }
+    })
+    disgusted.addEventListener('change', function() {
+        if (this.checked) {
+            emotion = 'disgusted'
+        }
+    })
+    scared.addEventListener('change', function() {
+        if (this.checked) {
+            emotion = 'fearful'
+        }
+    })
+    neutral.addEventListener('change', function() {
+        if (this.checked) {
+            emotion = 'neutral'
+        }
+    })
 }
 
 async function loadFacialRecognition() {
@@ -113,6 +179,5 @@ function detectEmotion() {
         } else {
             emotion = 'neutral'
         }
-        input.style.setProperty('--border', `var(--${emotion}color)`)
     }, 1000)
 }
